db70b25d8bca49b9a35499ecaf80a11e833af878
Ayman <Aymn.s@live.com>
2019-08-15 11:11:38 +0300

    Abstract shortest path code

---
+++ GuestLogix/GuestLogix.Core/Queries/GetShortestPath.cs
@@ -0,0 +1,25 @@
+﻿using GuestLogix.Core.DataAccess;
+
+namespace GuestLogix.Core.Queries
+{
+    public class GetShortestPath
+    {
+        private readonly IDbContext dbContext;
+
+        public GetShortestPath(IDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public string[] Handle(Request request)
+        {
+            return null;
+        }
+       
+        public class Request
+        {
+            public string Source { get; set; }
+            public string Destination { get; set; }
+        }
+    }
+}
---
+++ GuestLogix/GuestLogix.Web/Controllers/RouteController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuestLogix.Core.DataAccess;
+using GuestLogix.Core.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestLogix.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteController : ControllerBase
+    {
+        private readonly IDbContext dbContext;
+
+        public RouteController(IDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public string[] Get(string source, string destination)
+        {
+            var request = new GetShortestPath.Request
+            {
+                Source = source,
+                Destination = destination
+            };
+
+            return new GetShortestPath(dbContext).Handle(request);
+        }
+    }
+}
